7fb961ffbce1e5b72aaadf746545fc1604f76337
Tri Doan <w.dquoctri@gmail.com>
2022-05-12 16:47:46 +0700

    update script

--- remove.sh
+++ remove.sh
@@ -6,5 +6,5 @@ docker volume rm -f postgres-compose_pgadmin postgres-compose_pgdata;
 
 docker compose build --no-cache;
 
-read -n 1 -s -r -p "Press any key without 'power off' to continue!"
+read -n 1 -s -r -p "Press any key except 'power off' to continue!"
 exit 0;
